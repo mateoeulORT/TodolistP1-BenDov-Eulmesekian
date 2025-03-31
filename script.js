9633aeafc117e11027ebe8f0255f6054915401b7
mateoeulORT <48044168@est.ort.edu.ar>
2025-03-31 14:12:47 -0300

    god

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ tarea.descripcion
 tarea.fecha
 tarea.completada
 
-AgregarTarea()
+function VentanaTarea()
 {
-    
+    document.getElementById("lista").style.display = "none"
+    document.getElementById("agregrar-tarea").style.display = "block"
+}
+
+function Home(){
+    document.getElementById("lista").style.display = "block"
+    document.getElementById("agregrar-tarea").style.display = "none"
 }
